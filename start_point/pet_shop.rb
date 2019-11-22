1a22342909e15b48556a3ff969d9a5856a1478f0
Gary Clark <gclark04432@live.co.uk>
2019-11-22 14:43:49 +0000

    completed-non-optional

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -60,3 +60,11 @@ end
 def remove_customer_cash(customer, cash_amount)
   customer[:cash] -= cash_amount
 end
+
+def customer_pet_count(customer)
+  return customer[:pets].count()
+end
+
+def add_pet_to_customer(customer, new_pet)
+  customer[:pets].push(new_pet)
+end
